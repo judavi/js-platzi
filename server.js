c04478cca0bc57be719540a72ae47acce3016108
Juan David Gomez <judavi@gmail.com>
2016-09-27 08:39:37 -0500

    Added templates

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 var express = require('express');
 
 var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
-    res.send('hola mundo!');
-})
+  res.render('index');
+});
+
+app.get('/signup', function(req, res){
+  res.render('index');
+});
+
+app.get('/signin', function(req, res){
+  res.render('index');
+});
+
+app.listen(3000, function(err){
+  if(err){
+    console.log('Hubo un error');
+    process.exit(1);
+  }
+  console.log('Escuchando en 3000');
+});
